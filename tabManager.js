f91bf07e9b4395801b3c57c12a47694076d26374
jys923 <jys923@users.noreply.github.com>
2017-05-27 22:11:37 +0900

    Add files via upload

--- tabManager.js
+++ tabManager.js
@@ -58,70 +58,108 @@ function closeOtherTabs(info, tab) {
 	});
 }
 
-function gotoRightTabs(info, tab) {
+function gotoRightTab(info, tab) {
 	
-	chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-		chrome.tabs.move(tabs[0].id, {index : tabs[0].index++});
-		//chrome.tabs.move(tabs[0].id, {index : tabs[0].index++}, function() {
-		//	alert("move");
-		//});
+	var wTabs = new Array();
+	var tabs = new Array();
+	chrome.tabs.query({ currentWindow: true}, function (wTabs) {
+		chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+			var tabIDs = new Array();
+			for(var i = 0 ; i<wTabs.length ; i++){
+				if(tabs[0].index+1 == wTabs.length && wTabs[i].index==0){
+					chrome.tabs.update(wTabs[i].id, { active: true });
+				}else if(tabs[0].index+1 == wTabs[i].index){
+					chrome.tabs.update(wTabs[i].id, { active: true });
+				}
+			}
+		});
 	});
 }
 
-function gotoLeftTabs(info, tab) {
+function gotoLeftTab(info, tab) {
 	
-	chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-		chrome.tabs.move(tabs[0].id, {'index': tabs[0].index--});
+	var wTabs = new Array();
+	var tabs = new Array();
+	chrome.tabs.query({ currentWindow: true}, function (wTabs) {
+		chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+			var tabIDs = new Array();
+			for(var i = 0 ; i<wTabs.length ; i++){
+				//if(tabs[0].index-1 == 0 && wTabs[i].index+1==wTabs.length){
+				if(tabs[0].index == 0  && wTabs[i].index+1==wTabs.length ){
+					chrome.tabs.update(wTabs[i].id, { active: true });
+				}else if(tabs[0].index-1 == wTabs[i].index){
+					//chrome.tabs.update(wTabs[i].id, { highlighted: true });
+					chrome.tabs.update(wTabs[i].id, { active: true });
+				}
+			}
+		});
 	});
 }
 
-// Create one test item for each context type.
-/*chrome.contextMenus.create({
-  title: "close tabs", 
-  contexts:["page"], 
-  //onclick: closeRightTabs,
-});*/
 
 chrome.contextMenus.create({
   title: "->", 
-  contexts:["page"], 
+  contexts:["all"], 
   onclick: closeRightTabs,
 });
 
 chrome.contextMenus.create({
   title: "this", 
-  contexts:["page"], 
+  contexts:["all"], 
   onclick: closeThisTab,
 });
 
 chrome.contextMenus.create({
   title: "<-", 
-  contexts:["page"], 
+  contexts:["all"], 
   onclick: closeLeftTabs,
 });
 
 chrome.contextMenus.create({
   title: "Other", 
-  contexts:["page"], 
+  contexts:["all"], 
   onclick: closeOtherTabs,
 });
 
-/*
 chrome.contextMenus.create({
-  title: "goto", 
-  contexts:["page"], 
-  //onclick: closeRightTabs,
+  type: "separator", 
+  contexts:["all"], 
 });
 
 chrome.contextMenus.create({
-  title: "->", 
-  contexts:["page"], 
-  onclick: gotoRightTabs,
+  title: "-->", 
+  contexts:["all"], 
+  onclick: gotoRightTab,
 });
 
 chrome.contextMenus.create({
-  title: "<-", 
-  contexts:["page"], 
-  onclick: gotoLeftTabs,
+  title: "<--", 
+  contexts:["all"], 
+  onclick: gotoLeftTab,
 });
-*/
+
+/*chrome.contextMenus.create({
+  title: "<--", 
+  contexts:["page"]}, 
+  //onclick: gotoLeftTab,
+	function () {
+		
+		var wTabs = new Array();
+		var tabs = new Array();
+		chrome.tabs.query({ currentWindow: true}, function (wTabs) {
+			chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+				var tabIDs = new Array();
+				for(var i = 0 ; i<wTabs.length ; i++){
+					//if(tabs[0].index-1 == 0 && wTabs[i].index+1==wTabs.length){
+					if(tabs[0].index == 0  && wTabs[i].index+1==wTabs.length ){
+						chrome.tabs.update(wTabs[i].id, { active: true });
+					}else if(tabs[0].index-1 == wTabs[i].index){
+						//chrome.tabs.update(wTabs[i].id, { highlighted: true });
+						chrome.tabs.update(wTabs[i].id, { active: true });
+					}
+				}
+			});
+		});	
+	}
+);*/
+ 
